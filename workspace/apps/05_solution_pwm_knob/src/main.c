d38be399ff61b9ab4033e911946a1702357219c3
jainaneesh <jainaneesh15@gmail.com>
2025-06-05 22:18:45 +0000

    Cleaned up the code a bit more

--- workspace/apps/05_solution_pwm_knob/src/main.c
+++ workspace/apps/05_solution_pwm_knob/src/main.c
@@ -19,31 +19,36 @@ static const int32_t sleep_time_ms = 500;
 static const struct device *adc = DEVICE_DT_GET(DT_ALIAS(my_adc));
 static const struct adc_channel_cfg adc_ch = ADC_CHANNEL_CFG_DT(MY_ADC_CH);
 
-#if CONFIG_PWM_LED_COUNT == 3
+#if CONFIG_PWM_LED_COUNT >= 1
 	static const struct pwm_dt_spec pwm_led_0 = PWM_DT_SPEC_GET(DT_ALIAS(led_0));
+#endif
+
+#if CONFIG_PWM_LED_COUNT >= 2
 	static const struct pwm_dt_spec pwm_led_1 = PWM_DT_SPEC_GET(DT_ALIAS(led_1));
+#endif
+
+#if CONFIG_PWM_LED_COUNT >= 3
 	static const struct pwm_dt_spec pwm_led_2 = PWM_DT_SPEC_GET(DT_ALIAS(led_2));
 #endif
 
-#if CONFIG_PWM_LED_COUNT == 2
-	static const struct pwm_dt_spec pwm_led_0 = PWM_DT_SPEC_GET(DT_ALIAS(led_0));
-	static const struct pwm_dt_spec pwm_led_1 = PWM_DT_SPEC_GET(DT_ALIAS(led_1));
-#endif
-
-#if CONFIG_PWM_LED_COUNT == 1
-	static const struct pwm_dt_spec pwm_led_0 = PWM_DT_SPEC_GET(DT_ALIAS(led_0));
-#endif
 
 int main(void)
 {
-	int ret0;
-	int ret1;
-	int ret2;
 	uint16_t buf;
 	int32_t vref_mv;
-	uint32_t pulse_ns_0;
-	uint32_t pulse_ns_1;
-	uint32_t pulse_ns_2;
+
+	#if CONFIG_PWM_LED_COUNT >= 1
+		uint32_t pulse_ns_0;
+		int ret0;
+	#endif
+	#if CONFIG_PWM_LED_COUNT >= 2
+		uint32_t pulse_ns_1;
+		int ret1;
+	#endif
+	#if CONFIG_PWM_LED_COUNT >= 3
+		uint32_t pulse_ns_2;
+		int ret2;
+	#endif
 
 	// Get Vref (mV) from Devicetree property
 	vref_mv = DT_PROP(MY_ADC_CH, zephyr_vref_mv);
@@ -74,16 +79,12 @@ int main(void)
 		printk("Could not set up ADC\r\n");
 		return 0;
 	}
-	//printk("PWM_0 ready: %d\n", pwm_is_ready_dt(&pwm_led_0));
-	//printk("PWM_1 ready: %d\n", pwm_is_ready_dt(&pwm_led_1));
-	//printk("PWM_2 ready: %d\n", pwm_is_ready_dt(&pwm_led_2));
+
 	// Do forever
 	while (1) {
 
 		// Sample ADC
 		ret0 = adc_read(adc, &seq);
-		// ret1 = adc_read(adc, &seq);
-		// ret2 = adc_read(adc, &seq);
 		if (ret0 < 0) {
 			printk("Could not read ADC %d\r\n",ret0);
 			continue;
@@ -119,26 +120,28 @@ int main(void)
 		printk("Pulse: %u ns\r\n", pulse_ns);
 		#endif
 
-		// Set LED PWM pulse width
-		// ret0 = pwm_set_dt(&pwm_led_0, pwm_led_0.period, pulse_ns_0);
-		//printk("%d Period 0\n", pwm_led_0.period);
-		//printk("%d set to pulse width\n", ret0);
-		// ret1 = pwm_set_dt(&pwm_led_1, pwm_led_1.period, pulse_ns_0);
-		// ret1 = pwm_set_dt(&pwm_led_1, 1000, 500); // GPIO6 (CH1)
-		//printk("%d Period 1\n", pwm_led_1.period);
-		//printk("%d set to pulse width\n", pwm_led_1.period);
-		// ret2 = pwm_set_dt(&pwm_led_2, pwm_led_2.period, pulse_ns_0);
-		// ret2 = pwm_set_dt(&pwm_led_2, 1000, 500); // GPIO7 (CH2)
-		//printk("%d Period 2\n", pwm_led_2.period);
-		//printk("%d set to pulse width\n", ret2);
-		if (ret0 || ret1 || ret2) {
+		#if CONFIG_PWM_LED_COUNT >= 1
+		if (ret0 ) {
+			printk("Error %d: failed to set pulse width\n", ret0);
+			return 0;
+		}
+		#endif
+		#if CONFIG_PWM_LED_COUNT >= 2
+			if (ret0 || ret1) {
+			printk("Error %d %d: failed to set pulse width\n", ret0, ret1);
+			return 0;
+		}
+		#endif
+		#if CONFIG_PWM_LED_COUNT >= 3
+			if (ret0 || ret1 || ret2) {
 			printk("Error %d %d %d: failed to set pulse width\n", ret0, ret1, ret2);
 			return 0;
 		}
+		#endif
 
 		// Sleep
 		k_msleep(sleep_time_ms);
-	}
+		}
 
 	return 0;
 }
